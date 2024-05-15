86a1a6bf753b3c0780df3a59b466c2bcd64692a4
SlushGIS <122678016+SlushGIS@users.noreply.github.com>
2024-05-15 12:46:05 -0700

    Update main.js

--- Geo_Web_App_Development/Almanor_Water_Quality_App/main.js
+++ Geo_Web_App_Development/Almanor_Water_Quality_App/main.js
@@ -18,7 +18,7 @@ require(["esri/Map", "esri/views/MapView",
     const yearSlider = new Slider({
         container: "year",
         min: 2018,
-        max: 2023,
+        max: 2024,
         steps: 1,
         labelFormatFunction: function(value, type) {
             if (type === "value") {
@@ -26,7 +26,7 @@ require(["esri/Map", "esri/views/MapView",
             }
             return value;
         },
-        values: [2023],
+        values: [2024],
         visibleElements: {
             labels: true,
             rangeLabels: true
